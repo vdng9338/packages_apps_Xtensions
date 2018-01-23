3b8f0dace741cdfbf4bd47148c396c90305826a9
Joe Maples <joe@frap129.org>
2018-01-22 21:19:34 -0500

    Xtensions: Smart Pixels [2/2]
    Change-Id: I55775e02e849ad5db5b1187be0ca77b7c09f1081
    Signed-off-by: SonalSingh18 <sonal.singh.19993@gmail.com>

--- src/com/msm/xtended/fragments/SystemSettings.java
+++ src/com/msm/xtended/fragments/SystemSettings.java
@@ -64,6 +64,12 @@ public class SystemSettings extends SettingsPreferenceFragment implements
 
         final PreferenceScreen prefScreen = getPreferenceScreen();
 
+        boolean enableSmartPixels = getContext().getResources(). 
+                getBoolean(com.android.internal.R.bool.config_enableSmartPixels);
+        Preference SmartPixels = findPreference("smart_pixels"); if 
+        (!enableSmartPixels){
+            getPreferenceScreen().removePreference(SmartPixels);
+        }
     }
 
     @Override
